14f1bd228e0f9add5d9b446a60036672defd2dcd
Timofey_Babisashvili <timofey.babisashvili@gmail.com>
2023-10-01 14:20:40 +0300

    Header

--- src/Header/index.jsx
+++ src/Header/index.jsx
@@ -1,62 +1,38 @@
 import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+
 import { Link } from "react-router-dom";
 import Badge from "react-bootstrap/Badge";
-import moment from "moment";
-import { FORECAST_DATE_FORMAT } from "../constants";
 
-function Header({ list }) {
+function Header() {
   return (
     <Container className="navbar-text d-flex justify-content-center align-items-center container-center">
       <Navbar bg="white" variant="light" expand="sm">
-        <Container>
-          <Link to="/weather-app/" className="navbar-brand text-white ">
-            <Container className="ms-4">
-              <h1>
-                <Badge bg="success mt-2 ">
-                  WEATHER APP{" "}
-                  <img
-                    src="/weather-app/logo.png"
-                    width="30"
-                    height="30"
-                    className="d-inline-block align-top mt-0"
-                    alt="React Bootstrap logo"
-                  />
-                </Badge>
-              </h1>
-            </Container>
-          </Link>
-          <Container>
-            <Navbar.Toggle className="w-100" aria-controls="basic-navbar-nav" />
-          </Container>
+        <Link to="/weather-app/" className="navbar-brand text-white ">
+          <h1>
+            <Badge bg="success mt-2 ">
+              WEATHER APP{" "}
+              <img
+                src="/weather-app/logo.png"
+                width="30"
+                height="30"
+                className="d-inline-block align-top mt-0"
+                alt="React Bootstrap logo"
+              />
+            </Badge>
+          </h1>
+        </Link>
 
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className=" mt-2 ">
-              <Link className="nav-link header-style" to="/weather-app/contact">
-                Contact
-              </Link>
-              <NavDropdown
-                title="Forecast plan"
-                id="basic-nav-dropdown"
-                className="header-style"
-              >
-                {list?.map(({ dt }, index) => (
-                  <Link
-                    to={`/weather-app/forecast/${index}`}
-                    className="dropdown-item "
-                    key={index}
-                    // data-rr-ui-dropdown-item
-                  >
-                    {" "}
-                    {moment.unix(dt).format(FORECAST_DATE_FORMAT)}
-                  </Link>
-                ))}
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
+        <Link to="/weather-app/" className="navbar-brand text-white  ">
+          <Badge bg="success" className="mt-2 d-flex justify-content-center">
+            <Link
+              className="nav-link navbar-brand text-white p-0 m-0"
+              to="/weather-app/contact"
+            >
+              CONTACT
+            </Link>
+          </Badge>
+        </Link>
       </Navbar>
     </Container>
   );
